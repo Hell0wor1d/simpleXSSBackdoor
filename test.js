9e9320793bf30bf6e2e17e0d907d26faf409b8a3
Kev7n <root@kev7n.com>
2015-06-01 13:05:21 +0800

    init

--- test.js
+++ test.js
@@ -1,14 +1,6 @@
-addScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js')
-
-function addScript(src ) {
-    var s = document.createElement( 'script' );
-    s.setAttribute( 'src', src );
-    document.body.appendChild( s );
-}
-
 $.ajax({
     type: 'POST',
-    url: 'https://neo.pearson.com/__services/v2/rest/stream-config/user/associations/manage',
+    url: '/__services/v2/rest/stream-config/user/associations/manage',
     data: '[{"objectType":"3","objectID":"94242"}]',
     success: function(data){
         console.log(data)
