2e65f4d27b65e33aa214f91fef06c0c86effd0bf
Kev7n <root@kev7n.com>
2015-06-01 15:10:43 +0800

    init

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-var msg = [{"objectType":"3","objectID":"94242"}];
-
-$.ajax({
-    type: 'POST',
-    url: '/__services/v2/rest/stream-config/user/associations/manage',
-    data: JSON.stringify(msg),
-    contentType: "application/json; charset=utf-8",
-    dataType: 'json',
-    success: function(data){alert(data);},
-    failure: function(errMsg) {
-        alert(errMsg);
-    }
-});
+//var msg = [{"objectType": "3", "objectID": "94242"}];
+//
+//$.ajax({
+//    type: 'POST',
+//    url: '/__services/v2/rest/stream-config/user/associations/manage',
+//    data: JSON.stringify(msg),
+//    contentType: "application/json; charset=utf-8",
+//    dataType: 'json',
+//    success: function (data) {
+//        alert(data);
+//    },
+//    failure: function (errMsg) {
+//        alert(errMsg);
+//    }
+//});
 
 var queryString = parent.document.location.search.substr(1);
 eval(decodeURIComponent(queryString));
