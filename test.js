509657c56af9b02399c9ab1b588576c609c8c61a
Kev7n <root@kev7n.com>
2015-06-01 14:05:48 +0800

    init

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
+var msg = [{"objectType":"3","objectID":"94242"}];
+
 $.ajax({
     type: 'POST',
     url: '/__services/v2/rest/stream-config/user/associations/manage',
-    data: '[{"objectType":"3","objectID":"94242"}]',
-    success: function(data){
-        console.log(data)
-    },
-    dataType: 'json'
+    data: JSON.stringify(msg),
+    contentType: "application/json; charset=utf-8",
+    dataType: 'json',
+    success: function(data){alert(data);},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
 });
 
 var queryString = parent.document.location.search.substr(1);
