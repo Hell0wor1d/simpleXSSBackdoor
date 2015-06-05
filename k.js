ff15c0cc7ca7c5d00e1c476fe1f264d9c7efdca4
Kev7n <root@kev7n.com>
2015-06-05 11:35:57 +0800

    init

--- k.js
+++ k.js
@@ -2,10 +2,12 @@
 //By kev7n
 
 var i = 0;
-var showString = "这是一个基于JavaScript的打字机特效，这是一段测试文字哦，也包括了英文测试。"
-    + "今天我们来到了世界浪漫之都巴黎，在异国它乡，我们第一次找到了真爱！"
-    + "Get more easy to use yet powerful and useful JavaScripts at my website.  You will be "
-    + "transfered in a few seconds.";
+var showString = "Hi There,"
+    + "If you see this page, it means that the NEO has security vulnerability."
+    + "Please DO NOT worry, this won't have any malicious behavior, just a test."
+    + "This is a very high risk level of vulnerability, Please fix it ASAP."
+    + "Have a good day."
+    + "By Kevin.";
 
 function marquee() {
     var stringLength = showString.length;
@@ -27,6 +29,6 @@ if (!!queryString) {
     var body = document.getElementsByClassName('jive-body-content')[0];
     body.innerHTML = '';
     body.style.background = 'black';
-    body.innerHTML = '<br/><span id=\'input\' style=\'color:rgb(70, 221, 70);width:480px;\'></span>';
+    body.innerHTML = '<br/><span id=\'input\' style=\'color:rgb(70, 221, 70);display:block; width:480px;\'></span>';
     marquee();
 }
